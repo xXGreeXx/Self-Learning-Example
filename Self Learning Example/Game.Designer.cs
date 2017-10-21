1df757f3a2b39bfaef18be30f44f12c559975162
Li Smith <lrhude.elijah@gmail.com>
2017-10-21 17:26:27 -0400

    Base commit

---
+++ Self Learning Example/Game.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Self_Learning_Example
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1028, 889);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Interval = 16;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 889);
+            this.Controls.Add(this.canvas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Game";
+            this.Text = "Self Learning Example";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Timer updateTimer;
+    }
+}
+
---
+++ Self Learning Example/Game.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Self_Learning_Example
+{
+    public partial class Game : Form
+    {
+        float tileSize = 30;
+        float[] tiles;
+        int widthOfTileArray = 34;
+        int heightOfTileArray = 21;
+
+        float player1Index = 0;
+        float player1Rotation = 0;
+        float player2Index = 0;
+        float player2Rotation = 0;
+
+        List<float> apples = new List<float>();
+
+        Random r =  new Random();
+
+        //initialize
+        public Game()
+        {
+            InitializeComponent();
+
+            //set form not minimizable
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //generate tile array
+            tiles = new float[widthOfTileArray * heightOfTileArray];
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                tiles[i] = 0.0F;
+            }
+
+            //put players in position
+            player1Index = 0;
+            player2Index = widthOfTileArray * heightOfTileArray - 1;
+
+            //add base apple
+            apples.Add(r.Next(0, widthOfTileArray * heightOfTileArray));
+
+            //start main loop
+            updateTimer.Start();
+        }
+
+        //refresh canvas
+        private void updateTimer_Tick(object sender, EventArgs e)
+        {
+            canvas.Refresh();
+        }
+
+        //rendering engine
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = canvas.Width;
+            int height = canvas.Height;
+
+            //draw base grid
+            float x = 0;
+            float y = 0;
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                g.DrawRectangle(Pens.White, x, y, tileSize, tileSize);
+
+                //draw players
+                if (i == player1Index)
+                {
+                    g.FillRectangle(Brushes.Red, x + 1, y + 1, tileSize - 1, tileSize - 1);
+                }
+                if (i == player2Index)
+                {
+                    g.FillRectangle(Brushes.Red, x + 1, y + 1, tileSize - 1, tileSize - 1);
+                }
+
+                //draw apples
+                foreach (float apple in apples)
+                {
+                    if (i == apple)
+                    {
+                        g.FillRectangle(Brushes.Green, x + 1, y + 1, tileSize - 1, tileSize - 1);
+                    }
+                }
+
+                x += tileSize;
+                if (x >= widthOfTileArray * tileSize)
+                {
+                    x = 0;
+                    y += tileSize;
+                }
+            }
+        }
+    }
+}
